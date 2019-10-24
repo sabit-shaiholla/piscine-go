e105bb343e218dba76b5b207a5c9c7f137d23c3c
RedDevil06 <apple@RedDevil.local>
2019-10-24 18:53:59 +0600

    PrintWordsTables

---
+++ printwordstables.go
@@ -0,0 +1,13 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(table []string) {
+	for _, word := range table {
+		for _, i := range word {
+			z01.PrintRune(i)
+		}
+		z01.PrintRune(10)
+	}
+
+}
